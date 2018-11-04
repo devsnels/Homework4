228c3a4833afab7f1a83bfad5f0ead515f6b2dbd
Devin Nelson <devin.nelson@colostate.edu>
2018-11-04 16:32:01 -0700

    change variable name to be unique from dataset name

--- Homework4.R
+++ Homework4.R
@@ -16,15 +16,15 @@ head(homicide)
 homicide <- homicide %>% 
   unite(cityname, city, state, sep = ", ")
 
-homicide %>% 
+unsolved <- homicide %>% 
   select(cityname, disposition) %>% 
   mutate(disposition = factor(disposition)) %>% 
   mutate(disposition = fct_recode(disposition,  '1' = 'Closed without arrest', '1' = 'Open/No arrest', '0' = 'Closed by arrest')) %>%
   group_by(cityname) %>% 
   mutate(totalhomicides = n()) %>% 
-  mutate(unsolved = disposition == '1') %>% 
-  mutate(unsolved = sum(unsolved)) %>% 
-  group_by(cityname, unsolved) %>% 
+  mutate(totalunsolved = disposition == '1') %>% 
+  mutate(totalunsolved = sum(totalunsolved)) %>% 
+  group_by(cityname, totalunsolved) %>% 
   count() %>% 
   ungroup()
 
